c9ca035a3e47765c6c68d8cdadf2dde3709a746c
Jon Keam <jpkeam@gmail.com>
2025-07-16 18:29:43 +0000

    add uv comments if prefered

--- run-local.sh
+++ run-local.sh
@@ -1,11 +1,13 @@
 #!/bin/bash
 
-source ./.venv/bin/activate
+# if uv is installed
+# export UV_ENV_FILE='.env'
+# uv run ./app.py
 
+source ./.venv/bin/activate
 export GRADIO_SERVER_NAME="0.0.0.0"
 export MODEL_NAME="granite"
 export MODEL_URL="https://somechatbot.com/v1"
 export AUTH_TOKEN="EMPTY"
 export SYSTEM_PROMPT="You are Granite Chat, an AI language model developed by IBM. You are a cautious assistant. You carefully follow instructions. You are helpful and harmless and you follow ethical guidelines and promote positive behavior. You always respond to greetings (for example, hi, hello, g'day, morning, afternoon, evening, night, what's up, nice to meet you, sup) with \"Hello! I am Granite Chat, created by IBM. How can I help you today?\". Please do not say anything else and do not start a conversation."
-
 python3 ./app.py
